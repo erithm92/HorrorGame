258b92da75aa615b313170de9437e7c01c3f3bdd
Vennittor <ventor_prime@hotmail.com>
2015-10-30 13:39:48 -0700

    Teacher Spawn and chase calibrated

--- HorrorExp/Assets/Teacher/Hallway.cs
+++ HorrorExp/Assets/Teacher/Hallway.cs
@@ -25,25 +25,41 @@ public class Hallway : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.gameObject.name == "Player")
+		if(other.gameObject.transform.parent.name == "Player")
 		{
 			inHall = other.gameObject;
+
 			if(!teacherActive)
 			{
 				GameObject spawnLocation;
 				int randomMax = teacherSpawnLocations.Count;
 
-				spawnLocation = teacherSpawnLocations[Random.Range(0, randomMax)];
+				//check distance to spawn, if too close, roll again
+				bool spawnOK = false;
+				while (spawnOK == false)
+				{
+					spawnLocation = teacherSpawnLocations[Random.Range(0, randomMax)];
 
-				teacher.SetActive(true);
-				teacher.transform.position = spawnLocation.transform.position;
+					//check distance of player to spawn location
+					float distance = Vector3.Distance(spawnLocation.transform.position, other.gameObject.transform.position);
+
+					//if outside the range, spawnOK = true
+					if( distance > 25f)
+					{
+						spawnOK = true;
+
+						teacher.SetActive(true);
+						teacher.transform.position = spawnLocation.transform.position;
+					}
+				}
 			}
+
 		}
 	}
 
 	void OnTriggerExit(Collider other)
 	{
-		if(other.gameObject.name == "Player")
+		if(other.gameObject.transform.parent.name == "Player")
 		{
 			inHall = null;
 		}
